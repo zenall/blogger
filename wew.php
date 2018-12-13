84e3222c6d099c162c3ebade9ed0f61a7aaf5ed9
zenall <45296597+zenall@users.noreply.github.com>
2018-12-13 16:12:51 +0100

    Create wew.php

---
+++ wew.php
@@ -0,0 +1,28 @@
+<?php
+$mirror = isset($_GET['mirror']) ? $_GET['mirror'] : 'somedefault';
+    $json = file_get_contents('http://localhost/mp4u/get.php?sd=bi3kessge3qw');
+    $data = json_decode($json,true);
+    $hd = $data['720p'];
+    $sd = $data['480p'];
+    $img = $data['poster'];
+    $mirror1 =  ('sources: [{type: "video/mp4",file:"'.$hd.'",label: "High"},{file: "'.$sd.'",label: "Medium"}]');
+    $track1 = ('tracks: [{label: "Indonesia",file: "https://cdn.staticaly.com/gh/zenall/subtitle/master/Black%20Clover%20-%2001.srt",kind:"captions"}]');
+if (isset($_GET['mirror']) && $_GET['mirror'] == "1") {
+    $json = file_get_contents('http://localhost/mp4u/get.php?sd=bi3kessge3qw');
+    $data = json_decode($json,true);
+    $hd = $data['720p'];
+    $sd = $data['480p'];
+    $img = $data['poster'];
+    $mirror1 =  ('sources: [{type: "video/mp4",file:"'.$hd.'",label: "High"},{file: "'.$sd.'",label: "Medium"}]');
+    $track1 = ('tracks: [{label: "Indonesia",file: "https://cdn.staticaly.com/gh/zenall/subtitle/master/Black%20Clover%20-%2001.srt",kind:"captions"}]');
+  }
+if (isset($_GET['mirror']) && $_GET['mirror'] == "2") {
+    $json = file_get_contents('http://localhost/mp4u/get_sendit.php?sd=thillhu591yk&hd=thillhu591yk');
+    $data = json_decode($json,true);
+    $hd = $data['720p'];
+    $sd = $data['480p'];
+    $img = $data['poster'];
+    $mirror1 =  ('sources: [{type: "video/mp4",file:"'.$hd.'",label: "High"},{file: "'.$sd.'",label: "Medium"}]');
+    $track1 = ('tracks: [{label: "Indonesia",file: "",kind:"captions"}]');
+  }
+?>
