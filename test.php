b321174899a1e91834617bd9ef97ef4f4e66b668
zenall <45296597+zenall@users.noreply.github.com>
2018-12-13 17:54:15 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,15 @@
-<div id="myList">https://videos.whatchado.com/ali-mahlodji-mp4-fullhd-58594a314526e.mp4</div>
+<html>
+    <head>
+    <script type="text/javascript">
+    function load()
+    {
+    window.location.href = "https://videos.whatchado.com/ali-mahlodji-mp4-fullhd-58594a314526e.mp4";
+
+    }
+    </script>
+    </head>
+
+    <body onload="load()">
+    <h1>Hello World!</h1>
+    </body>
+    </html> 
