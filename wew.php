caf95ff40bc2faf5e4afb827f37dc1748878c199
zenall <45296597+zenall@users.noreply.github.com>
2018-12-13 16:14:52 +0100

    Update wew.php

--- wew.php
+++ wew.php
@@ -26,3 +26,35 @@ if (isset($_GET['mirror']) && $_GET['mirror'] == "2") {
     $track1 = ('tracks: [{label: "Indonesia",file: "",kind:"captions"}]');
   }
 ?>
+  <script type="text/javascript">
+jwplayer("player").setup({
+  mediaid: "",
+  title: "ChanStream",
+  description: "Streaming Anime Subtitle Indonesia",
+  image:
+    "<?php echo $img?>",
+  localization: {
+    cc: "Untertitel"
+  },
+  <?php echo $mirror1?>,
+  <?php echo $track1?>,
+  skin: {
+    name: "chan",
+    url: "chan.css"
+  },
+  "logo": {
+    "file": "https://i.ibb.co/K2zvQHq/chanstream.png",
+    "hide": false,
+    "link": "#",
+    "margin": "10",
+    "position": "control-bar"
+  },
+  abouttext: 'ChanStream',
+  aboutlink: '#',
+    captions: {
+        color: '#FFF',
+        fontSize: 10,
+        fontFamily: 'Tahoma'
+      }
+});      
+  </script>
